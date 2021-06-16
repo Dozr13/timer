14afff22b71729135d20c716769bc2fdef06b484
Wade Pate <wadejp8@gmail.com>
2021-06-16 16:12:09 -0600

    Changed to functional component

--- src/App.js
+++ src/App.js
@@ -1,82 +1,126 @@
-import React, { useState, useEffect } from "react";
+import React, { Component, createRef } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  const [timerActive, setTimerActive] = useState(false);
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    };
+    this.hoursInput = createRef();
+    this.minutesInput = createRef();
+    this.secondsInput = createRef();
+  }
 
-  const [seconds, setSeconds] = useState("00");
-  const [minutes, setMinutes] = useState("00");
-  const [hours, setHours] = useState("00");
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    let intervalId;
-
-    if (timerActive) {
-      intervalId = setInterval(() => {
-        const secondCounter = counter % 60;
-        const minuteCounter = Math.floor(counter / 60);
-        const hourCounter = Math.floor(counter / 60 / 60);
-
-        const intSeconds =
-          String(secondCounter).length === 1
-            ? `0${secondCounter}`
-            : secondCounter;
-        const intMinutes =
-          String(minuteCounter).length === 1
-            ? `0${minuteCounter}`
-            : minuteCounter;
-
-        const intHours =
-          String(hourCounter).length === 1 ? `0${hourCounter}` : hourCounter;
-
-        setSeconds(intSeconds);
-        setMinutes(intMinutes);
-
-        setHours(intHours);
-
-        setCounter((counter) => counter + 1);
-      }, 1000);
-    }
-
-    return () => clearInterval(intervalId);
-  }, [timerActive, counter]);
-
-  const handleTimerReset = () => {
-    setTimerActive(false);
-    setSeconds("00");
-    setMinutes("00");
-    setHours("00");
+  inputHandler = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
   };
 
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <main className='timer-container'>
-          <section className='timer-top-section'>
-            <div className='timer-display'>
-              {`${hours} : ${minutes} : ${seconds}`}
+  convertToSeconds = (hours, minutes, seconds) => {
+    return seconds + minutes * 60 + hours * 60 * 60;
+  };
+
+  startTimer = () => {
+    this.timer = setInterval(this.countDown, 1000);
+  };
+
+  countDown = () => {
+    const { hours, minutes, seconds } = this.state;
+    let convertSeconds = this.convertToSeconds(hours, minutes, seconds);
+
+    if (convertSeconds) {
+      seconds
+        ? this.setState({ seconds: seconds - 1 })
+        : this.setState({ seconds: 59 });
+
+      if (convertSeconds % 60 === 0 && minutes) {
+        this.setState({ minutes: minutes - 1 });
+      }
+
+      if (!minutes && hours) {
+        this.setState({ minutes: 59 });
+      }
+
+      if (convertSeconds % 3600 === 0 && hours) {
+        this.setState({ hours: hours - 1 });
+      }
+    } else {
+      clearInterval(this.timer);
+    }
+  };
+
+  stopTimer = () => {
+    clearInterval(this.timer);
+  };
+
+  resetTimer = () => {
+    this.setState({
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    });
+    this.hoursInput.current.value = 0;
+    this.minutesInput.current.value = 0;
+    this.secondsInput.current.value = 0;
+  };
+
+  render() {
+    const { hours, minutes, seconds } = this.state;
+
+    return (
+      <main className='App'>
+        <div className='logo-container'>
+          <img src={logo} className='App-logo' alt='logo' />
+        </div>
+        <body className='App-body'>
+          <section className='timer-container'>
+            <div className='countdown-display'>
+              <p>-Timer-</p>
+              {hours}:{minutes}:{seconds}
             </div>
-          </section>
-          <section className='timer-bottom-section'>
-            <div className='timer-button-container'>
-              <button
-                className={timerActive ? "pause-button" : "start-button"}
-                onClick={() => setTimerActive(!timerActive)}
-              >
-                {timerActive ? "Pause" : "Start"}
+            <div className='input-container'>
+              <input
+                ref={this.hoursInput}
+                type='number'
+                placeholder='Hours'
+                name='hours'
+                onChange={this.inputHandler}
+              />
+              <input
+                ref={this.minutesInput}
+                type='number'
+                placeholder='Minutes'
+                name='minutes'
+                onChange={this.inputHandler}
+              />
+              <input
+                ref={this.secondsInput}
+                type='number'
+                placeholder='Seconds'
+                name='seconds'
+                onChange={this.inputHandler}
+              />
+            </div>
+
+            <div className='button-container'>
+              <button onClick={this.startTimer} className='start-button'>
+                Start
               </button>
-              <button className='reset-button' onClick={handleTimerReset}>
+              <button onClick={this.stopTimer} className='pause-button'>
+                Pause
+              </button>
+              <button onClick={this.resetTimer} className='reset-button'>
                 Reset
               </button>
             </div>
           </section>
-        </main>
-      </header>
-    </div>
-  );
+        </body>
+      </main>
+    );
+  }
 }
 
 export default App;
